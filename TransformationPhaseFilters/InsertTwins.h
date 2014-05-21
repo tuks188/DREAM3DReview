bb45d69d9a9a8002e2b18195be9721ca4471880f
Joseph Tucker <joetucker@alumni.cmu.edu>
2014-05-21 16:17:11 -0400

    edited header file for attributeMatrix compatibility

--- TransformationPhaseFilters/InsertTwins.h
+++ TransformationPhaseFilters/InsertTwins.h
@@ -39,18 +39,19 @@
 #define INSERTTWINS_H_
 
 #include <vector>
-#include <string>
+#include <QtCore/QString>
 
+#include <boost/shared_array.hpp>
 
 #include "DREAM3DLib/DREAM3DLib.h"
 #include "DREAM3DLib/Common/DREAM3DSetGetMacros.h"
 #include "DREAM3DLib/DataArrays/IDataArray.h"
 
 #include "DREAM3DLib/Common/AbstractFilter.h"
-#include "DREAM3DLib/DataContainers/VoxelDataContainer.h"
+#include "DREAM3DLib/DataContainers/VolumeDataContainer.h"
 #include "DREAM3DLib/OrientationOps/OrientationOps.h"
 #include "DREAM3DLib/DataArrays/NeighborList.hpp"
-
+#include "Plugins/SyntheticBuilding/SyntheticBuildingConstants.h"
 
 /**
  * @class InsertTwins InsertTwins.h DREAM3DLib/SyntheticBuildingFilters/InsertTwins.h
@@ -59,8 +60,9 @@
  * @date May 21, 2014
  * @version 1.0
  */
-class DREAM3DLib_EXPORT InsertTwins : public AbstractFilter
+class InsertTwins : public AbstractFilter
 {
+    Q_OBJECT /* Need this for Qt's signals and slots mechanism to work */
   public:
     DREAM3D_SHARED_POINTERS(InsertTwins)
     DREAM3D_STATIC_NEW_MACRO(InsertTwins)
@@ -68,39 +70,70 @@ class DREAM3DLib_EXPORT InsertTwins : public AbstractFilter
 
     virtual ~InsertTwins();
 
-    //------ Required Cell Data
-    DREAM3D_INSTANCE_STRING_PROPERTY(GrainIdsArrayName)
-    DREAM3D_INSTANCE_STRING_PROPERTY(CellEulerAnglesArrayName)
-    //------ Created Cell Data
-    //------ Required Field Data
-    DREAM3D_INSTANCE_STRING_PROPERTY(AvgQuatsArrayName)
-    DREAM3D_INSTANCE_STRING_PROPERTY(CentroidsArrayName)
-    DREAM3D_INSTANCE_STRING_PROPERTY(EquivalentDiametersArrayName)
-    DREAM3D_INSTANCE_STRING_PROPERTY(FieldEulerAnglesArrayName)
-    DREAM3D_INSTANCE_STRING_PROPERTY(FieldPhasesArrayName)
-    //------ Created Field Data
-    DREAM3D_INSTANCE_STRING_PROPERTY(ActiveArrayName)
-    DREAM3D_INSTANCE_STRING_PROPERTY(FieldParentIdsArrayName)
-	DREAM3D_INSTANCE_STRING_PROPERTY(NumGrainsPerParentArrayName)
-    //------ Required Ensemble Data
-    DREAM3D_INSTANCE_STRING_PROPERTY(CrystalStructuresArrayName)
-    DREAM3D_INSTANCE_STRING_PROPERTY(PhaseTypesArrayName)
-    DREAM3D_INSTANCE_STRING_PROPERTY(ShapeTypesArrayName)
-    //------ Created Ensemble Data
-    DREAM3D_INSTANCE_STRING_PROPERTY(NumFieldsArrayName)
+    DREAM3D_FILTER_PARAMETER(float, TwinThickness)
+    Q_PROPERTY(float TwinThickness READ getTwinThickness WRITE setTwinThickness)
+    DREAM3D_FILTER_PARAMETER(int, NumTwinsPerGrain)
+    Q_PROPERTY(int NumTwinsPerGrain READ getNumTwinsPerGrain WRITE setNumTwinsPerGrain)
+    DREAM3D_FILTER_PARAMETER(float, CoherentFrac)
+    Q_PROPERTY(float CoherentFrac READ getCoherentFrac WRITE setCoherentFrac)
+    DREAM3D_FILTER_PARAMETER(float, PeninsulaFrac)
+    Q_PROPERTY(float PeninsulaFrac READ getPeninsulaFrac WRITE setPeninsulaFrac)
 
-    DREAM3D_INSTANCE_PROPERTY(float, TwinThickness)
-    DREAM3D_INSTANCE_PROPERTY(int, NumTwinsPerGrain)
-    DREAM3D_INSTANCE_PROPERTY(float, CoherentFrac)
-    DREAM3D_INSTANCE_PROPERTY(float, PeninsulaFrac)
+    DREAM3D_FILTER_PARAMETER(DataArrayPath, NewCellFeatureAttributeMatrixName)
+    Q_PROPERTY(DataArrayPath NewCellFeatureAttributeMatrixName READ getNewCellFeatureAttributeMatrixName WRITE setNewCellFeatureAttributeMatrixName)
 
-    virtual const std::string getGroupName() { return DREAM3D::FilterGroups::SyntheticBuildingFilters; }
-	virtual const std::string getSubGroupName() {return DREAM3D::FilterSubGroups::PackingFilters;}
-    virtual const std::string getHumanLabel() { return "Insert Twins"; }
+    DREAM3D_FILTER_PARAMETER(DataArrayPath, FeatureIdsArrayPath)
+    Q_PROPERTY(DataArrayPath FeatureIdsArrayPath READ getFeatureIdsArrayPath WRITE setFeatureIdsArrayPath)
+    
+	DREAM3D_FILTER_PARAMETER(DataArrayPath, CellEulerAnglesArrayPath)
+    Q_PROPERTY(DataArrayPath CellEulerAnglesArrayPath READ getCellEulerAnglesArrayPath WRITE setCellEulerAnglesArrayPath)
+
+    DREAM3D_FILTER_PARAMETER(DataArrayPath, AvgQuatsArrayPath)
+    Q_PROPERTY(DataArrayPath AvgQuatsArrayPath READ getAvgQuatsArrayPath WRITE setAvgQuatsArrayPath)
+
+    DREAM3D_FILTER_PARAMETER(DataArrayPath, CentroidsArrayPath)
+    Q_PROPERTY(DataArrayPath CentroidsArrayPath READ getCentroidsArrayPath WRITE setCentroidsArrayPath)
+
+    DREAM3D_FILTER_PARAMETER(QString, EquivalentDiametersArrayName)
+    Q_PROPERTY(QString EquivalentDiametersArrayName READ getEquivalentDiametersArrayName WRITE setEquivalentDiametersArrayName)
+
+    DREAM3D_FILTER_PARAMETER(QString, FeatureEulerAnglesArrayName)
+    Q_PROPERTY(QString FeatureEulerAnglesArrayName READ getFeatureEulerAnglesArrayName WRITE setFeatureEulerAnglesArrayName)
+
+    DREAM3D_FILTER_PARAMETER(QString, FeaturePhasesArrayName)
+    Q_PROPERTY(QString FeaturePhasesArrayName READ getFeaturePhasesArrayName WRITE setFeaturePhasesArrayName)
+
+    DREAM3D_FILTER_PARAMETER(QString, ActiveArrayName)
+    Q_PROPERTY(QString ActiveArrayName READ getActiveArrayName WRITE setActiveArrayName)
+
+    DREAM3D_FILTER_PARAMETER(DataArrayPath, FeatureParentIdsArrayPath)
+    Q_PROPERTY(DataArrayPath FeatureParentIdsArrayPath READ getFeatureParentIdsArrayPath WRITE setFeatureParentIdsArrayPath)
+	
+    DREAM3D_FILTER_PARAMETER(DataArrayPath, CrystalStructuresArrayPath)
+    Q_PROPERTY(DataArrayPath CrystalStructuresArrayPath READ getCrystalStructuresArrayPath WRITE setCrystalStructuresArrayPath)
+
+    DREAM3D_FILTER_PARAMETER(QString, PhaseTypesArrayName)
+    Q_PROPERTY(QString PhaseTypesArrayName READ getPhaseTypesArrayName WRITE setPhaseTypesArrayName)
+
+    DREAM3D_FILTER_PARAMETER(QString, ShapeTypesArrayName)
+    Q_PROPERTY(QString ShapeTypesArrayName READ getShapeTypesArrayName WRITE setShapeTypesArrayName)
+
+    DREAM3D_FILTER_PARAMETER(QString, NumFeaturesPerParentArrayName)
+    Q_PROPERTY(QString NumFeaturesPerParentArrayName READ getNumFeaturesPerParentArrayName WRITE setNumFeaturesPerParentArrayName)
+
+    virtual const QString getCompiledLibraryName() { return SyntheticBuilding::SyntheticBuildingBaseName; }
+    virtual AbstractFilter::Pointer newFilterInstance(bool copyFilterParameters);
+	virtual const QString getGroupName() { return DREAM3D::FilterGroups::SyntheticBuildingFilters; }
+	virtual const QString getSubGroupName() {return DREAM3D::FilterSubGroups::PackingFilters;}
+    virtual const QString getHumanLabel() { return "Insert Twins"; }
+    virtual const QString getBrandingString() { return SyntheticBuilding::SyntheticBuildingPluginDisplayName + " Filter"; }
 
     virtual void setupFilterParameters();
-    virtual int writeFilterParameters(AbstractFilterParametersWriter* writer, int index);
-    
+    /**
+    * @brief This method will write the options to a file
+    * @param writer The writer that is used to write the options to a file
+    */
+    virtual int writeFilterParameters(AbstractFilterParametersWriter* writer, int index);  
     /**
     * @brief This method will read the options from a file
     * @param reader The reader that is used to read the options from a file
@@ -113,36 +146,46 @@ class DREAM3DLib_EXPORT InsertTwins : public AbstractFilter
     virtual void execute();
     virtual void preflight();
 
+signals:
+    void updateFilterParameters(AbstractFilter* filter);
+    void parametersChanged();
+    void preflightAboutToExecute();
+    void preflightExecuted();
+
+  protected:
+    InsertTwins();
+
 	void insert_twins();
 	bool place_twin(size_t curGrain, float sample111[], size_t totalFields, float plateThickness, float d, size_t numGrains);
     void peninsula_twin(size_t curGrain, size_t totalFields);
 	size_t transfer_attributes(size_t totalFields, size_t totalPoints, QuatF q, float e[], size_t curGrain);
 	void filter_calls();
 
-  protected:
-    InsertTwins();
-
   private:
-    int32_t* m_GrainIds;
-    float* m_CellEulerAngles;
-    float* m_AvgQuats;
-    bool* m_Active;
-    float* m_Centroids;
-    float* m_EquivalentDiameters;
-	float* m_FieldEulerAngles;
-	int32_t* m_FieldPhases;
-	int32_t* m_FieldParentIds;
-	int32_t* m_NumGrainsPerParent;
 
-    unsigned int* m_CrystalStructures;
-    unsigned int* m_PhaseTypes;
-    unsigned int* m_ShapeTypes;
-    int32_t* m_NumFields;
+    // Cell Data - make sure these are all initialized to NULL in the constructor
+    DEFINE_REQUIRED_DATAARRAY_VARIABLE(int32_t, FeatureIds)
+	DEFINE_REQUIRED_DATAARRAY_VARIABLE(float, CellEulerAngles)
 
-    std::vector<OrientationOps::Pointer> m_OrientationOps;
+    // Feature Data - make sure these are all initialized to NULL in the constructor
+	DEFINE_REQUIRED_DATAARRAY_VARIABLE(float, AvgQuats)
+	DEFINE_CREATED_DATAARRAY_VARIABLE(bool, Active)
+    DEFINE_REQUIRED_DATAARRAY_VARIABLE(float, Centroids)
+    DEFINE_REQUIRED_DATAARRAY_VARIABLE(float, EquivalentDiameters)
+    DEFINE_REQUIRED_DATAARRAY_VARIABLE(float, FieldEulerAngles)
+    DEFINE_REQUIRED_DATAARRAY_VARIABLE(int32_t, FeaturePhases)
+    DEFINE_CREATED_DATAARRAY_VARIABLE(int32_t, FeatureParentIds)
+	DEFINE_CREATED_DATAARRAY_VARIABLE(int32_t, NumFeaturesPerParent)
 
+    // Ensemble Data - make sure these are all initialized to NULL in the constructor
+    DEFINE_REQUIRED_DATAARRAY_VARIABLE(unsigned int, CrystalStructures)
+    DEFINE_REQUIRED_DATAARRAY_VARIABLE(uint32_t, PhaseTypes)
+    DEFINE_REQUIRED_DATAARRAY_VARIABLE(uint32_t, ShapeTypes)
+	DEFINE_CREATED_DATAARRAY_VARIABLE(int32_t, NumFields)
+
+    // All other private variables
     void dataCheck(bool preflight, size_t voxels, size_t fields, size_t ensembles);
-
+    void updateFeatureInstancePointers();
 
     InsertTwins(const InsertTwins&); // Copy Constructor Not Implemented
     void operator=(const InsertTwins&); // Operator '=' Not Implemented
