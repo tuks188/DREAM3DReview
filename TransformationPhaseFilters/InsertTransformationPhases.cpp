be99ea228c163a0c69c0216d6819b1622e65b270
Joey Kleingers <joey.kleingers@bluequartz.net>
2015-06-08 15:53:04 -0400

    Adding missing includes.

--- TransformationPhaseFilters/InsertTransformationPhases.cpp
+++ TransformationPhaseFilters/InsertTransformationPhases.cpp
@@ -52,7 +52,7 @@
 #include "DREAM3DLib/FilterParameters/AbstractFilterParametersReader.h"
 #include "DREAM3DLib/FilterParameters/ChoiceFilterParameter.h"
 #include "DREAM3DLib/FilterParameters/LinkedBooleanFilterParameter.h"
-
+#include "DREAM3DLib/FilterParameters/SeparatorFilterParameter.h"
 
 #include "OrientationLib/OrientationMath/OrientationMath.h"
 #include "OrientationLib/OrientationMath/OrientationArray.hpp"
