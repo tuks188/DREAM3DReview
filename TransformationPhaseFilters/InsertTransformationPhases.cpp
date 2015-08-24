6a2a37a84bb7f11d2c4b6d87c3f5455fe15ff131
Sean Donegan <sean.donegan@bluequartz.net>
2015-08-24 14:39:51 -0400

    Fix incorrect getting of an array during data check
    The InsertTransformationPhase filter was attempting to create an array during
    data check that was actually supposed to be an input array.  Fixed by swapping
    the create function to a get function.

--- TransformationPhaseFilters/InsertTransformationPhases.cpp
+++ TransformationPhaseFilters/InsertTransformationPhases.cpp
@@ -409,7 +409,7 @@ void InsertTransformationPhases::dataCheck()
   if( NULL != m_ShapeTypesPtr.lock().get() ) /* Validate the Weak Pointer wraps a non-NULL pointer to a DataArray<T> object */
   { m_ShapeTypes = m_ShapeTypesPtr.lock()->getPointer(0); } /* Now assign the raw pointer to data from the DataArray<T> object */
 
-  m_NumFeaturesPtr = getDataContainerArray()->createNonPrereqArrayFromPath<DataArray<int32_t>, AbstractFilter, int32_t>(this,  getNumFeaturesArrayPath(), 0, dims); /* Assigns the shared_ptr<> to an instance variable that is a weak_ptr<> */
+  m_NumFeaturesPtr = getDataContainerArray()->getPrereqArrayFromPath<DataArray<int32_t>, AbstractFilter>(this,  getNumFeaturesArrayPath(), dims); /* Assigns the shared_ptr<> to an instance variable that is a weak_ptr<> */
   if( NULL != m_NumFeaturesPtr.lock().get() ) /* Validate the Weak Pointer wraps a non-NULL pointer to a DataArray<T> object */
   { m_NumFeatures = m_NumFeaturesPtr.lock()->getPointer(0); } /* Now assign the raw pointer to data from the DataArray<T> object */
 }
