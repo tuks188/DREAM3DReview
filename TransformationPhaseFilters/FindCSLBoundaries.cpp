99ccaacce4cbd844fa8d876663f09c8403af607b
Michael Jackson <mike.jackson@bluequartz.net>
2015-10-06 09:34:54 -0400

    Updating function with new API that takes into account active or passive transformations

--- TransformationPhaseFilters/FindCSLBoundaries.cpp
+++ TransformationPhaseFilters/FindCSLBoundaries.cpp
@@ -50,7 +50,6 @@
 #include "SIMPLib/Math/QuaternionMath.hpp"
 #include "SIMPLib/Math/GeometryMath.h"
 #include "SIMPLib/FilterParameters/AbstractFilterParametersWriter.h"
-
 #include "SIMPLib/FilterParameters/DoubleFilterParameter.h"
 #include "SIMPLib/FilterParameters/DataArraySelectionFilterParameter.h"
 #include "SIMPLib/FilterParameters/StringFilterParameter.h"
@@ -153,7 +152,7 @@ class CalculateCSLBoundaryImpl
               m_OrientationOps[phase1]->getQuatSymOp(j, sym_q);
               //calculate crystal direction parallel to normal
               QuaternionMathF::Multiply(misq, sym_q, s1_misq);
-              QuaternionMathF::MultiplyQuatVec(sym_q, xstl_norm, s_xstl_norm);
+              QuaternionMathF::MultiplyQuatVec(sym_q, xstl_norm, s_xstl_norm, Rotations::Constants::epsijk);
               for (int k = 0; k < nsym; k++)
               {
                 //calculate the symmetric misorienation
