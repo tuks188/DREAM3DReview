f4480ee31db198d497899004d06c8d4f60086918
Joey Kleingers <joey.kleingers@bluequartz.net>
2016-08-05 11:06:22 -0400

    Replacing all DataArrayCreationFilterParameter::New function uses with its new macro.

--- TransformationPhaseFilters/InsertTransformationPhases.cpp
+++ TransformationPhaseFilters/InsertTransformationPhases.cpp
@@ -248,7 +248,7 @@ void InsertTransformationPhases::setupFilterParameters()
   parameters.push_back(StringFilterParameter::New("Parent Ids", "FeatureParentIdsArrayName", getFeatureParentIdsArrayName(), FilterParameter::CreatedArray, SIMPL_BIND_SETTER(InsertTransformationPhases, this, FeatureParentIdsArrayName), SIMPL_BIND_GETTER(InsertTransformationPhases, this, FeatureParentIdsArrayName)));
   {
     DataArrayCreationFilterParameter::RequirementType req = DataArrayCreationFilterParameter::CreateRequirement(SIMPL::AttributeMatrixObjectType::Feature);
-    parameters.push_back(DataArrayCreationFilterParameter::New("Number of Features Per Parent", "NumFeaturesPerParentArrayPath", getNumFeaturesPerParentArrayPath(), FilterParameter::CreatedArray, SIMPL_BIND_SETTER(InsertTransformationPhases, this, NumFeaturesPerParentArrayPath), SIMPL_BIND_GETTER(InsertTransformationPhases, this, NumFeaturesPerParentArrayPath), req));
+    parameters.push_back(SIMPL_NEW_DA_CREATION_FP("Number of Features Per Parent", NumFeaturesPerParentArrayPath, FilterParameter::CreatedArray, InsertTransformationPhases, req));
   }
 
   setFilterParameters(parameters);
