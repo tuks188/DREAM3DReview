1e26f6e5534b5d1e3645cfbd81615184cd23d7c9
Michael Jackson <mike.jackson@bluequartz.net>
2018-01-22 10:12:41 -0500

    Removing a comment that is unnecessary and confusing

--- TransformationPhasePlugin.cpp
+++ TransformationPhasePlugin.cpp
@@ -46,7 +46,7 @@
 
 #include "TransformationPhase/TransformationPhaseConstants.h"
 
-// Include the MOC generated CPP file which has all the QMetaObject methods/data
+
 
 // -----------------------------------------------------------------------------
 //
