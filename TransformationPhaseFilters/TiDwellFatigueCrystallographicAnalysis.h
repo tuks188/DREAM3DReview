47bbee2bb21976dd56a68646f6b3fc1a18a06ce8
Michael Jackson <mike.jackson@bluequartz.net>
2018-01-25 18:50:55 -0500

    Add override keyword to more places. By default quiet the warnings about inconsistent overrides.

--- TransformationPhaseFilters/TiDwellFatigueCrystallographicAnalysis.h
+++ TransformationPhaseFilters/TiDwellFatigueCrystallographicAnalysis.h
@@ -207,7 +207,7 @@ class TiDwellFatigueCrystallographicAnalysis : public AbstractFilter
     /**
      * @brief readFilterParameters Reimplemented from @see AbstractFilter class
      */
-    virtual void readFilterParameters(AbstractFilterParametersReader* reader, int index);
+    virtual void readFilterParameters(AbstractFilterParametersReader* reader, int index) override;
 
     /**
     * @brief readFilterParametersFromJson Reads the filter parameters from a file
