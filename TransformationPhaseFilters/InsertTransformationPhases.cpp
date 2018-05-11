40a1ce76de75a8f653aed029b64dcc228bb1985b
Michael Jackson <mike.jackson@bluequartz.net>
2018-05-10 20:56:26 -0400

    no message

--- TransformationPhaseFilters/InsertTransformationPhases.cpp
+++ TransformationPhaseFilters/InsertTransformationPhases.cpp
@@ -334,9 +334,10 @@ void InsertTransformationPhases::dataCheck()
 
   DataContainer::Pointer m = getDataContainerArray()->getPrereqDataContainer(this, m_FeatureIdsArrayPath.getDataContainerName(), false);
   if(getErrorCondition() < 0 || m == nullptr) { return; }
-  AttributeMatrix::Pointer statsGenAttrMat = getDataContainerArray()->getPrereqAttributeMatrixFromPath<AbstractFilter>(this, getStatsGenCellEnsembleAttributeMatrixPath(), -301);
+
+  AttributeMatrix::Pointer statsGenAttrMat = m->getPrereqAttributeMatrix(this, getStatsGenCellEnsembleAttributeMatrixPath().getAttributeMatrixName(), -301);
   if(getErrorCondition() < 0 || statsGenAttrMat == nullptr) { return; }
-  AttributeMatrix::Pointer volAttrMat = getDataContainerArray()->getPrereqAttributeMatrixFromPath<AbstractFilter>(this, getNumFeaturesPerParentArrayPath(), -301);
+  AttributeMatrix::Pointer volAttrMat = m->getPrereqAttributeMatrix(this, getNumFeaturesPerParentArrayPath().getAttributeMatrixName(), -301);
   if(getErrorCondition() < 0 || volAttrMat == nullptr) { return; }
 
   ImageGeom::Pointer image = m->getPrereqGeometry<ImageGeom, AbstractFilter>(this);
