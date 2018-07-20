e041004cc089eee4aa6755c94c40fb2d9d22facb
Michael Jackson <mike.jackson@bluequartz.net>
2018-07-20 10:04:44 -0400

    Update plugin information that is displayed to the user

--- MASSIFUtilitiesPlugin.cpp
+++ MASSIFUtilitiesPlugin.cpp
@@ -11,22 +11,28 @@
 #include "SIMPLib/Filtering/FilterManager.h"
 
 #include "MASSIFUtilities/MASSIFUtilitiesConstants.h"
+#include "MASSIFUtilities/MASSIFUtilitiesVersion.h"
 
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
-MASSIFUtilitiesPlugin::MASSIFUtilitiesPlugin() :
-m_Version("0.1.0"),                            // Initialize MASSIFUtilities's Version Number Here
-m_CompatibilityVersion("0.1.0"), // Initialize MASSIFUtilities's Compatibility Version Number Here
-m_Vendor("Vendor Name"),                                // Initialize MASSIFUtilities's Vendor Name Here
-m_URL("URL"),                                           // Initialize Company URL Here
-m_Location("Location"),                                 // Initialize MASSIFUtilities library Location Here
-m_Description("Description"),                           // Initialize MASSIFUtilities's Description Here
-m_Copyright("Copyright"),                               // Initialize MASSIFUtilities's Copyright Here
-m_Filters(QList<QString>()),                        // Initialize MASSIFUtilities's List of Dependencies Here
-m_DidLoad(false)
+MASSIFUtilitiesPlugin::MASSIFUtilitiesPlugin()
+: m_Version(MASSIFUtilities::Version::Package())
+, m_CompatibilityVersion(MASSIFUtilities::Version::Package())
+, m_Vendor("Open-Source")
+, // Initialize MASSIFUtilities's Vendor Name Here
+    m_URL("http://www.github.com/dream3D/MASSIFUtilities")
+, // Initialize Company URL Here
+    m_Location("")
+, // Initialize MASSIFUtilities library Location Here
+    m_Description("")
+, // Initialize MASSIFUtilities's Description Here
+    m_Copyright("")
+, // Initialize MASSIFUtilities's Copyright Here
+    m_Filters(QList<QString>())
+, // Initialize MASSIFUtilities's List of Dependencies Here
+    m_DidLoad(false)
 {
-
 }
 
 // -----------------------------------------------------------------------------
@@ -110,9 +116,9 @@ QString MASSIFUtilitiesPlugin::getDescription()
   QFileInfo licenseFileInfo(licenseFile);
   QString text = "<<--Description was not read-->>";
 
-  if ( licenseFileInfo.exists() )
+  if(licenseFileInfo.exists())
   {
-    if ( licenseFile.open(QIODevice::ReadOnly | QIODevice::Text) )
+    if(licenseFile.open(QIODevice::ReadOnly | QIODevice::Text))
     {
       QTextStream in(&licenseFile);
       text = in.readAll();
@@ -141,9 +147,9 @@ QString MASSIFUtilitiesPlugin::getLicense()
   QFileInfo licenseFileInfo(licenseFile);
   QString text = "<<--License was not read-->>";
 
-  if ( licenseFileInfo.exists() )
+  if(licenseFileInfo.exists())
   {
-    if ( licenseFile.open(QIODevice::ReadOnly | QIODevice::Text) )
+    if(licenseFile.open(QIODevice::ReadOnly | QIODevice::Text))
     {
       QTextStream in(&licenseFile);
       text = in.readAll();
@@ -160,18 +166,18 @@ QMap<QString, QString> MASSIFUtilitiesPlugin::getThirdPartyLicenses()
   QMap<QString, QString> licenseMap;
   QList<QString> fileStrList;
   fileStrList.push_back(":/ThirdParty/HDF5.txt");
-  
+
   fileStrList.push_back(":/ThirdParty/Qt.txt");
   fileStrList.push_back(":/ThirdParty/Qwt.txt");
 
-  for (QList<QString>::iterator iter = fileStrList.begin(); iter != fileStrList.end(); iter++)
+  for(QList<QString>::iterator iter = fileStrList.begin(); iter != fileStrList.end(); iter++)
   {
     QFile file(*iter);
     QFileInfo licenseFileInfo(file);
 
-    if ( licenseFileInfo.exists() )
+    if(licenseFileInfo.exists())
     {
-      if ( file.open(QIODevice::ReadOnly | QIODevice::Text) )
+      if(file.open(QIODevice::ReadOnly | QIODevice::Text))
       {
         QTextStream in(&file);
         licenseMap.insert(licenseFileInfo.baseName(), in.readAll());
